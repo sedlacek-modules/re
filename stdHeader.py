3ee30a8b5c1d9c968dd256ea42f0acb2b37083a5
Jaromir Sedlacek <jaromir.sedlacek@gmail.com>
2013-11-26 18:30:49 +0100

    Standard copyright and license headr to be added to all source files

---
+++ stdHeader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+ (C) Copyright 2013, Jaromir Sedlacek (jaromir.sedlacek@gmail.com).
+
+ All rights reserved.
+
+ This is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License (LGPL) as
+ published by the Free Software Foundation; either version 3.0 of
+ the License, or (at your option) any later version.
+
+ This software is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU LGPL http://www.gnu.org/licenses/lgpl.html.
+"""
